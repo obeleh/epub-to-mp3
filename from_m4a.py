9ced735eb08afc47327b826c083dd5df323ee792
Sjuul Janssen <sjuul@outlook.com>
2024-08-21 06:36:03 +0200

    .

--- from_m4a.py
+++
@@ -1,7 +0,0 @@
-from pydub import AudioSegment
-
-m4a_file = 'm4a/Akkerstraat.m4a'
-wav_filename = 'm4a/Akkerstraat.wav'
-
-sound = AudioSegment.from_file(m4a_file, format='m4a')
-file_handle = sound.export(wav_filename, format='wav')
